e02f38213af157af9cfeb124d9d132d8e2725387
Andrey Goncharov <andrey@goncharov.page>
2020-10-25 11:53:38 +0300

    Implement SourceCodeExplorer

--- src/source-code-explorer.ts
+++ src/source-code-explorer.ts
@@ -1,5 +1,21 @@
+import { readFile } from 'fs/promises'
+import { resolve } from 'path'
+
+interface GetFileOptions {
+  encoding?: BufferEncoding
+}
+
 export class SourceCodeExplorer {
   constructor(public readonly repoPath: string) {}
 
-  public async getFile(relativePath: string): Promise<string | undefined> {}
+  public async getFile(relativePath: string, { encoding }: GetFileOptions = {}): Promise<string | undefined> {
+    try {
+      const fileFullPath = resolve(this.repoPath, relativePath)
+
+      const content = await readFile(fileFullPath, { encoding: encoding ?? 'utf-8', flag: 'r' })
+      return content
+    } catch {
+      return undefined
+    }
+  }
 }
